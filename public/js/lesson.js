c63d2f51e9fb515c0acd7772cc32967e9220e457
Youssef ElKhattabe <kharbouchitv@gmail.com>
2022-03-28 01:26:50 +0000

    Fixes

--- public/js/lesson.js
+++ public/js/lesson.js
@@ -9,9 +9,9 @@ window.onload = function (){
     vocabulary  = externalData.vocabulary;
     summary_test = externalData.summary_test
 
-    lesson_title =  document.getElementById("lesson-title")
+    lesson_title =  document.getElementById("lesson_title")
     activities_container = document.getElementById("activities-container")
-    lesson_image = document.getElementById("lesson-img")
+    lesson_image = document.getElementById("lesson_img")
 
     //? LESSONS INFO INJECTION
     lesson_title.innerText = info.title
@@ -19,38 +19,48 @@ window.onload = function (){
 
     //? ACTIVITIES INJECTION
     //! EXERCISES
-    exercises.forEach((exercise , index) => {
-        elem = document.createElement("div");
-        elem.innerHTML = `<a href="/exercise/${exercise.externalId}"><h3>Exercise ${index + 1}</h3>  <img src="XXX" alt="img">`
-        elem.className = "item-exercise";
-        activities_container.appendChild(elem)
-    }) 
+    if(exercises.length > 0){
+        exercises.forEach((exercise , index) => {
+            elem = document.createElement("div");
+            elem.innerHTML = `<i class="fa-solid fa-pen-to-square fa-xl"></i> <a href="/exercise/${exercise.externalId}"><h3>Exercise ${index + 1}</h3></a>`
+            elem.className = "item_exercise";
+            activities_container.appendChild(elem)
+        }) 
+    }
+    
     //! VIDEOS
+    if(videos.length > 0){
     videos.forEach((video , index) => {
-        elem = document.createElement("div");
-        elem.innerHTML = `<a href="/video/${video.externalId}"><h3>Video ${index + 1}</h3></a> <img src="XXX" alt="img">`
-        elem.className = "item-video";
-        activities_container.appendChild(elem)
-    }) 
+            elem = document.createElement("div");
+            elem.innerHTML = `<i class="fa-solid fa-video fa-xl"></i> <a href="/video/${video.externalId}"><h3>Video ${index + 1}</h3></a>`
+            elem.className = "item_video";
+            activities_container.appendChild(elem)
+        }) 
+    }
     //! GRAMMAR RULES
-    grammarRules.forEach((rule , index) => {
-        elem = document.createElement("div");
-        elem.innerHTML = `<a href="/grammar_rule/${rule.externalId}"><h3>Grammar Rule ${index + 1}</h3><img src="XXX" alt="img">`
-        elem.className = "item-rule";
-        activities_container.appendChild(elem)
-    }) 
+    if(grammarRules.length > 0){
+        grammarRules.forEach((rule , index) => {
+            elem = document.createElement("div");
+            elem.innerHTML = `<i class="fa-solid fa-spell-check fa-xl"></i> <a href="/grammar_rule/${rule.externalId}"><h3>Grammar Rule ${index + 1}</h3>`
+            elem.className = "item_rule";
+            activities_container.appendChild(elem)
+        }) 
+    }
     //! VOCABULARY
-    vocabulary.forEach((voc , index) => {
-        elem = document.createElement("div");
-        elem.innerHTML = `<a href="/vocabulary/${voc.externalId}"><h3>Vocabulary ${index + 1}</h3><img src="XXX" alt="img">`
-        elem.className = "item-vocabulary";
-        activities_container.appendChild(elem)
-    }) 
-    //! SUMMARY TEST 
+    if(vocabulary.length > 0){
+        vocabulary.forEach((voc , index) => {
+            elem = document.createElement("div");
+            elem.innerHTML = `<i class="fa-brands fa-alipay fa-xl"></i> <a href="/vocabulary/${voc.externalId}"><h3>Vocabulary ${index + 1}</h3>`
+            elem.className = "item_vocabulary";
+            activities_container.appendChild(elem)
+        })
+    }
+     
+    //! SUMMARY TEST
     if(summary_test.length > 0){
         elem = document.createElement("div");
-        elem.innerHTML = `<a href="/exercise/${summary_test[0].externalId}"><h3>Summary Test</h3><img src="XXX" alt="img">`
-        elem.className = "item-summary";
+        elem.innerHTML = `<i class="fa-solid fa-book-bookmark fa-xl"></i> <a href="/exercise/${summary_test[0].externalId}"><h3>Summary Test</h3>`
+        elem.className = "item_summary";
         activities_container.appendChild(elem)
     }
     
