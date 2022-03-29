af155fc9bce0462b899a872c13f8b9da9333a3e8
itszeusx <kharbouchitv@gmail.com>
2022-03-29 18:15:33 +0000

    Fixs

--- public/js/lesson.js
+++ public/js/lesson.js
@@ -23,7 +23,23 @@ window.onload = function (){
     if(videos.length > 0){
         videos.forEach((video , index) => {
                 elem = document.createElement("div");
-                elem.innerHTML = `<i class="fa-solid fa-video fa-xl"></i> <a href="/video/${video.externalId}"><h3>Video ${index + 1}</h3></a>`
+                elem.innerHTML = `
+                <a class="item" href="/video/${video.externalId}">
+                    <div class="item_container">
+                        <div class="item_info">
+                        <lord-icon
+                        src="https://cdn.lordicon.com/axyzxviq.json"
+                        trigger="morph"
+                        colors="primary:#25C8DB"
+                        style="width:100px;height:60px">
+                    </lord-icon>
+                    
+                            <div class="item_title">Video</div>
+                        </div>
+                        <div class="item_index">${index +1}</div>
+                    </div>
+                </a>
+                `
                 elem.className = "item_video";
                 activities_container.appendChild(elem)
             }) 
@@ -32,17 +48,45 @@ window.onload = function (){
     if(grammarRules.length > 0){
         grammarRules.forEach((rule , index) => {
             elem = document.createElement("div");
-            elem.innerHTML = `<i class="fa-solid fa-spell-check fa-xl"></i> <a href="/grammar_rule/${rule.externalId}"><h3>Grammar Rule ${index + 1}</h3>`
+            elem.innerHTML = 
+            `
+                <a class="item" href="/grammar_rule/${rule.externalId}">
+                    <div class="item_container">
+                        <div class="item_info">
+                            <img class="item_image" src="/assets/icons/grammar_rule.png" alt="">
+                            <div class="item_title">Grammar Rule</div>
+                        </div>
+                        <div class="item_index">${index +1}</div>
+                    </div>
+                </a>
+                `
             elem.className = "item_rule";
             activities_container.appendChild(elem)
         }) 
     }
 
     //! EXERCISES
+    // elem.innerHTML = `<i class="fa-solid fa-pen-to-square fa-xl"></i> <a href="/exercise/${exercise.externalId}"><h3>Exercise ${index + 1}</h3></a>`
+    // elem.className = "item_exercise";
     if(exercises.length > 0){
         exercises.forEach((exercise , index) => {
             elem = document.createElement("div");
-            elem.innerHTML = `<i class="fa-solid fa-pen-to-square fa-xl"></i> <a href="/exercise/${exercise.externalId}"><h3>Exercise ${index + 1}</h3></a>`
+            elem.innerHTML = `
+            <a class="item" href="/exercise/${exercise.externalId}">
+                <div class="item_container">
+                    <div class="item_info">
+                    <lord-icon
+                    src="https://cdn.lordicon.com/keogyrep.json"
+                    trigger="morph"
+                    colors="primary:#C07BE1"
+                    style="width:100px;height:60px">
+                </lord-icon>
+                        <div class="item_title">Exercise</div>
+                    </div>
+                    <div class="item_index">${index +1}</div>
+                </div>
+            </a>
+            `
             elem.className = "item_exercise";
             activities_container.appendChild(elem)
         }) 
@@ -52,7 +96,17 @@ window.onload = function (){
     if(vocabulary.length > 0){
         vocabulary.forEach((voc , index) => {
             elem = document.createElement("div");
-            elem.innerHTML = `<i class="fa-brands fa-alipay fa-xl"></i> <a href="/vocabulary/${voc.externalId}"><h3>Vocabulary ${index + 1}</h3>`
+            elem.innerHTML = `
+            <a class="item" href="/vocabulary/${voc.externalId}">
+                <div class="item_container">
+                    <div class="item_info">
+                        <img class="item_image" src="/assets/icons/vocabulary.png" alt="">
+                        <div class="item_title">Vocabulary</div>
+                    </div>
+                    <div class="item_index">${index +1}</div>
+                </div>
+            </a>
+            `
             elem.className = "item_vocabulary";
             activities_container.appendChild(elem)
         })
@@ -61,7 +115,17 @@ window.onload = function (){
     //! SUMMARY TEST
     if(summary_test.length > 0){
         elem = document.createElement("div");
-        elem.innerHTML = `<i class="fa-solid fa-book-bookmark fa-xl"></i> <a href="/exercise/${summary_test[0].externalId}"><h3>Summary Test</h3>`
+        elem.innerHTML = `
+        <a class="item" href="/summary_test/${summary_test.externalId}">
+            <div class="item_container">
+                <div class="item_info">
+                    <img class="item_image" src="/assets/icons/summary_test.png" alt="">
+                    <div class="item_title">Summary Test</div>
+                </div>
+                <div class="item_index">1</div>
+            </div>
+        </a>
+        `
         elem.className = "item_summary";
         activities_container.appendChild(elem)
     }
@@ -70,7 +134,18 @@ window.onload = function (){
     if(practice.length > 0){
         practice.forEach((prac , index) => {
             elem = document.createElement("div");
-            elem.innerHTML = `<i class="fa-brands fa-alipay fa-xl"></i> <a href="/practice/${prac.externalId}"><h3>Practice</h3>`
+            elem.innerHTML = 
+            `
+            <a class="item" href="/practice/${prac.externalId}">
+                <div class="item_container">
+                    <div class="item_info">
+                        <img class="item_image" src="/assets/icons/practice.png" alt="">
+                        <div class="item_title">Practice</div>
+                    </div>
+                    <div class="item_index">${index + 1}</div>
+                </div>
+            </a>
+            `
             elem.className = "item_practice";
             activities_container.appendChild(elem)
         })
